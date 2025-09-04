80b04ff26fbab4ea710298d2e2235d2a48560c4b
bforddev7 <bford.lief@gmail.com>
2025-09-03 22:00:23 -0500

    setting up project base

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+// ---------------
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT;
+
+// ---------------
+// ---------------
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
